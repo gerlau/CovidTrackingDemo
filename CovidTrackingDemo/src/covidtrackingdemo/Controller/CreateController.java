49f6ec811bc22dd2f0869fe6cb084600a8803b21
bar8 <barryteo95@hotmail.com>
2021-04-27 21:33:32 +0800

    new update

--- CovidTrackingDemo/src/covidtrackingdemo/Controller/CreateController.java
+++ CovidTrackingDemo/src/covidtrackingdemo/Controller/CreateController.java
@@ -16,10 +16,38 @@ public class CreateController {
     
     public CreateController() {}
 
-    public void create(String username, String password, String privilege, String firstName, String lastName) throws IOException {
+    public int create(String username, String password, String privilege, String firstName, String lastName) throws IOException {
 
             HealthOrganization ho = new HealthOrganization();
             
-            ho.create(username, password, privilege, firstName, lastName);
+            int validationIsSuccessful = validate(username, password, privilege, firstName, lastName);
+
+            if (validationIsSuccessful == 0)
+                ho.create(username, password, privilege, firstName, lastName);
+            
+            return validationIsSuccessful;
+    }
+    
+    private int validate(String username, String password, String privilege, String firstName, String lastName) throws IOException {
+
+        String regex_password = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()+=])(?=\\S+$).{8,}$";
+
+        //Validate empty fields
+        if (username.equals("") || password.equals("") || firstName.equals("") || lastName.equals("")) {
+            return 1; 
+        }
+
+        //Validate email
+        else if (!username.contains("@")) {
+            return 2;
+        }
+
+        //Validate password
+        else if (!password.matches(regex_password)) {
+            return 3;
+        }
+        
+        else 
+            return 0;
     }
 }
